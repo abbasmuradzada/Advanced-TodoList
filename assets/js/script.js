42fb6a47d8d49c21a5ee6762630ed2c5332dda32
abbasmuradzada <abbasam@code.edu.az>
2020-11-18 14:09:25 +0400

    add, edit, drag-drop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,127 @@
 const todoList = document.querySelector('.todo-list');
 const completedList = document.querySelector('.completed-list');
+const todoInput = document.querySelector('.todo-input')
+const checkBtns = document.querySelectorAll('.check');
 const todoRow = document.querySelectorAll('.todo-row'); 
+const addBtn = document.querySelector('.add');
+const editInput = document.querySelector('.edit');
+const editBtn = document.querySelector('.edit-btn');
+const todoRank = document.querySelector('.rank');
 
-let num;
 
-document.querySelector('button').addEventListener('click',  ()=> {
-    var test = document.createElement('div');
-    test.className="todo-row";
-    test.id=`a${counter}`;
-    test.setAttribute('draggable', 'true')
+let elementId;
+let editedElementId;
+let counter=todoRow.length + 1;
+
+// CReate todoRow with input
+addBtn.addEventListener('click',  (event)=> {
+    event.preventDefault();
+    // add To Do Row
+    let row = document.createElement('div');
+    row.id=counter;
+    row.className = todoRank.value == 'important' ? "todo-row important" : "todo-row";
+    row.setAttribute('draggable', 'true')
+    todoList.appendChild(row);    
+    // add checkbox
+    let input = document.createElement('input');
+    input.setAttribute('type', 'checkbox')
+    input.className="check";
+    row.appendChild(input);
+    // add number
+    let num = document.createElement('span');
+    num.className="listNum";
+    num.innerText=`${counter}. `;
+    row.appendChild(num);
+    // add text content
+    let spanContent = document.createElement('span');
+    spanContent.innerText=` ${todoInput.value}`
+    spanContent.className="todoContent"
+    row.appendChild(spanContent);
+    // add edit btn
+    let editBtn = document.createElement('button');
+    editBtn.className=('edit');
+    editBtn.innerText='edit';
+    row.appendChild(editBtn);
+    // add delete btn
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className=('delete');
+    deleteBtn.innerText='x';
+    row.appendChild(deleteBtn);
+
+    todoInput.value='';
     counter++;
-    todoList.appendChild(test);
+    renderTodoList()
+    clickCheckBox();
+    deleteToDoRow();
+    editToDoRow();
 })
 
-todoRow.forEach(element => {
-    element.ondragstart = function(){
-        num=this.id
-    } 
-});
+// Remove Element
+var deleteToDoRow = () => {
+    const deleteBtns = document.querySelectorAll('.delete');
+    deleteBtns.forEach(element => {
+        element.addEventListener('click', () => {
+            element.parentElement.remove();
+        })
+    });
+}
 
+// Edit Element
+var editToDoRow = () => {
+    const editBtns = document.querySelectorAll('.edit');
+    editBtns.forEach(element => {
+        element.addEventListener('click', () => {
+            editBtn.removeAttribute('disabled');
+            editInput.value = element.previousElementSibling.innerText;
+            editInput.focus();
+            editedElementId=element.parentElement.id;
+        })
+    });
+}
+editToDoRow();
+
+editBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.getElementById(editedElementId).querySelector('.todoContent').innerText = editInput.value;
+    editInput.value='';
+    editBtn.setAttribute('disabled', 'true');
+    editInput.blur();
+
+})
+
+// In drag starting take element 
+var renderTodoList = () => {
+    const todoRow = document.querySelectorAll('.todo-row'); 
+    todoRow.forEach(element => {
+        element.ondragstart = function(){
+            elementId=this.id
+        }
+    });    
+}
+renderTodoList()
+
+// Click CheckBox and change TodoWow Position
+var clickCheckBox = () => {
+    let checkedInput = document.querySelectorAll('.check');
+    checkedInput.forEach(element => {
+        element.addEventListener('click', ()=>{
+            setTimeout(() => {
+                if (element.checked) {
+                    completedList.append(element.parentElement);
+                    element.parentElement.querySelector('.edit').style.visibility = "hidden";
+                    element.parentElement.querySelector('.delete').style.visibility = "hidden";
+                }else{
+                    todoList.append(element.parentElement);
+                    element.parentElement.querySelector('.edit').style.visibility = "visible";
+                    element.parentElement.querySelector('.delete').style.visibility = "visible";
+                }
+            }, 300);
+        })
+    });
+}
+clickCheckBox();
+
+//Clear drag over
 completedList.ondragover = (event) => {
     event.preventDefault();
 }
@@ -27,13 +130,19 @@ todoList.ondragover = (event) => {
     event.preventDefault();
 }
 
+// In dropping, element append
 completedList.ondrop = (event) => {
-    event.target.append(document.getElementById(num));
+    const draggedElement = document.getElementById(elementId);
+    completedList.append(draggedElement);
+    draggedElement.querySelector('.check').checked=true;
+    draggedElement.querySelector('.edit').style.visibility = "hidden";
+    draggedElement.querySelector('.delete').style.visibility = "hidden";
 }
 
 todoList.ondrop = (event) => {
-    event.target.append(document.getElementById(num));
+    const draggedElement = document.getElementById(elementId);
+    todoList.append(draggedElement);
+    draggedElement.querySelector('.check').checked=false;
+    draggedElement.querySelector('.edit').style.visibility = "visible";
+    draggedElement.querySelector('.delete').style.visibility = "visible";
 }
-var counter=1;
-
-
